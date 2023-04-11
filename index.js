5910f2b97e0b66b654e798a1494561ed023541cd
Awesome Kalin <47223207+AwesomeKalin@users.noreply.github.com>
2023-04-11 17:18:06 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,245 @@
+function kalhashcycle(x, k) {
+var a = x[0] * 2, b = x[1] * 3, c = x[2] * 3, d = x[3] * 4, e = x[4] * 5, f = x[5] * 6;
+
+a = aa(a, b+1, c-1, d, k[0], 7, -993949367);
+d = aa(d*2, f+2, b-2, c/2, k[1], 12, -873195708);
+c = aa(c*3, e+3, f-3, b/3, k[2], 17,  -806131953);
+b = aa(b*4, a+4, e-4, f/4, k[3], 22, -691672692);
+f = aa(f*5, d+5, a-5, e/5, k[4], 27, 58156827);
+e = aa(e*6, c+6, d-6, a/6, k[5], 32,  -852561922);
+a = aa(a*7, b+7, c-7, d/7, k[6], 7, 505826877);
+d = aa(d*8, f+8, b-8, c/8, k[7], 12, -257582494);
+c = aa(c*9, e+9, f-9, b/9, k[8], 17,  -391076224);
+b = aa(b*10, a+10, e-10, f/10, k[9], 22, 237012518);
+f = aa(f*11, d+11, a-11, e/11, k[10], 27, 280094750);
+e = aa(e*12, c+12, d-12, a/12, k[11], 32, -553611280);
+a = aa(a*13, b+13, c-13, d/13, k[12], 7,  -824567476);
+d = aa(d*14, f+14, b-14, c/14, k[13], 12, 221605167);
+c = aa(c*15, e+15, f-15, b/15, k[14], 17, 221066262);
+b = aa(b*16, a+16, e-16, f/16, k[15], 22,  899373800);
+f = aa(f*17, d+17, a-17, e/17, k[16], 27,  403881548);
+e = aa(e*18, c+18, d-18, a/18, k[17], 32,  573331986);
+
+a = bb(a, b, c+1, d-1, k[14], 5, 994408175);
+d = bb(d/2, f*2, b+2, c-2, k[11], 9, 513389660);
+c = bb(c/3, e*3, f+3, b-3, k[8], 14,  152458963);
+b = bb(b/4, a*4, e+4, f-4, k[9], 20, -514982672);
+f = bb(f/5, d*5, a+5, e-5, k[3], 27, -964000660);
+e = bb(e/6, c*6, d+6, a-6, k[2], 35,  906699503);
+a = bb(a/7, b*7, c+7, d-7, k[5], 5, -606522726);
+d = bb(d/8, f*8, b+8, c-8, k[0], 9, 984093010);
+c = bb(c/9, e*9, f+9, b-9, k[13], 14,  244344156);
+b = bb(b/10, a*10, e+10, f-10, k[12], 20, 756415686);
+f = bb(f/11, d*11, a+11, e-11, k[1], 27, 613301999);
+e = bb(e/12, c*12, d+12, a-12, k[15], 35,  -690210614);
+a = bb(a/13, b*13, c+13, d-13, k[10], 5, 267737564);
+d = bb(d/14, f*14, b+14, c-14, k[7], 9, 778329336);
+c = bb(c/15, e*15, f+15, b-15, k[17], 14,  -892366398);
+b = bb(b/16, a*16, e+16, f-16, k[16], 20, -127248784);
+f = bb(f/17, d*17, a+17, e-17, k[4], 27, 885043229);
+e = bb(e/18, c*18, d+18, a-18, k[6], 35, -220026997);
+
+a = cc(a-1, b, c, d+1, k[11], 4, 710529346);
+d = cc(d-2, f/2, b*2, c+2, k[17], 11, -531724794);
+c = cc(c-3, e/3, f*3, b+3, k[1], 16,  712950800);
+b = cc(b-4, a/4, e*4, f+4, k[16], 23, -35054413);
+f = cc(f-5, d/5, a*5, e+5, k[12], 28, -949177368);
+e = cc(e-6, c/6, d*6, a+6, k[2], 35,  590809461);
+a = cc(a-7, b/7, c*7, d+7, k[5], 4, -334521374);
+d = cc(d-8, f/8, b*8, c+8, k[15], 11, -760746102);
+c = cc(c-9, e/9, f*9, b+9, k[9], 16, -388300398);
+b = cc(b-10, a/10, e*10, f+10, k[4], 23, 445188849);
+f = cc(f-11, d/11, a*11, e+11, k[7], 28, 749828011);
+e = cc(e-12, c/12, d*12, a+12, k[0], 35,  975323886);
+a = cc(a-13, b/13, c*13, d+13, k[8], 4, -40901590);
+d = cc(d-14, f/14, b*14, c+14, k[14], 11, -836798136);
+c = cc(c-15, e/15, f*15, b+15, k[6], 16,  -298130555);
+b = cc(b-16, a/16, e*16, f+16, k[3], 23, 294515168);
+f = cc(f-17, d/17, a*17, e+17, k[10], 28, 904398394);
+e = cc(e-18, c/18, d*18, a+18, k[13], 35, 983880257);
+
+a = dd(a+1, b-1, c, d, k[17], 6, -198630844);
+d = dd(d+2, f-2, b/2, c*2, k[16], 10,  1126891415);
+c = dd(c+3, e-3, f/3, b*3, k[10], 15, -1416354905);
+b = dd(b+4, a-4, e/4, f*4, k[1], 21, -57434055);
+f = dd(f+5, d-5, a/5, e*5, k[12], 28,  1700485571);
+e = dd(e+6, c-6, d/6, a*6, k[8], 36, -1894986606);
+a = dd(a+7, b-7, c/7, d*7, k[13], 6, -1051523);
+d = dd(d+8, f-8, b/8, c*8, k[15], 10, -2054922799);
+c = dd(c+9, e-9, f/9, b*9, k[9], 15,  1873313359);
+b = dd(b+10, a-10, e/10, f*10, k[11], 21, -30611744);
+f = dd(f+11, d-11, a/11, e*11, k[3], 28, -1560198380);
+e = dd(e+12, c-12, d/12, a*12, k[14], 36,  1309151649);
+a = dd(a+13, b-13, c/13, d*13, k[7], 6, -145523070);
+d = dd(d+14, f-14, b/14, c*14, k[4], 10, -1120210379);
+c = dd(c+15, e-15, f/15, b*15, k[6], 15,  718787259);
+b = dd(b+16, a-16, e/16, f*16, k[0], 21, -343485551);
+f = dd(f+17, d-17, a/17, e*17, k[2], 28, -343485551);
+e = dd(e+18, c-18, d/18, a*18, k[5], 36, -343485551);
+
+a = ee(a, b+1, c-1, d, k[14], 6, -204192766);
+d = ee(d*2, f+2, b-2, c/2, k[17], 10,  -110995339);
+c = ee(c*3, e+3, f-3, b/3, k[9], 15, -563559643);
+b = ee(b*4, a+4, e-4, f/4, k[0], 21, -655960839);
+f = ee(f*5, d+5, a-5, e/5, k[1], 28,  -263345228);
+e = ee(e*6, c+6, d-6, a/6, k[2], 36, 711223296);
+a = ee(a*7, b+7, c-7, d/7, k[11], 6, -307107035);
+d = ee(d*8, f+8, b-8, c/8, k[16], 10, -224197626);
+c = ee(c*9, e+9, f-9, b/9, k[7], 15,  864435296);
+b = ee(b*10, a+10, e-10, f/10, k[4], 21, -886357967);
+f = ee(f*11, d+11, a-11, e/11, k[8], 28, 190485133);
+e = ee(e*12, c+12, d-12, a/12, k[3], 36,  -615263137);
+a = ee(a*13, b+13, c-13, d/13, k[10], 6, 2215553);
+d = ee(d*14, f+14, b-14, c/14, k[5], 10, -419776589);
+c = ee(c*15, e+15, f-15, b/15, k[12], 15,  832104074);
+b = ee(b*16, a+16, e-16, f/16, k[13], 21, -668171015);
+f = ee(f*17, d+17, a-17, e/17, k[9], 28,  738805289);
+e = ee(e*18, c+18, d-18, a/18, k[15], 36, 980109206);
+
+// Need to do last numbers
+
+a = ff(a, b, c+1, d-1, k[9], 6, -931864290);
+d = ff(d/2, f*2, b+2, c-2, k[17], 10,  95124165);
+c = ff(c/3, e*3, f+3, b-3, k[8], 15, -83700404);
+b = ff(b/4, a*4, e+4, f-4, k[0], 21, 413212964);
+f = ff(f/5, d*5, a+5, e-5, k[12], 28,  -630256713);
+e = ff(e/6, c*6, d+6, a-6, k[7], 36, -611980089);
+a = ff(a/7, b*7, c+7, d-7, k[11], 6, 505172536);
+d = ff(d/8, f*8, b+8, c-8, k[2], 10, 296598838);
+c = ff(c/9, e*9, f+9, b-9, k[5], 15,  -548847659);
+b = ff(b/10, a*10, e+10, f-10, k[3], 21, 485417903);
+f = ff(f/11, d*11, a+11, e-11, k[15], 28, -216087257);
+e = ff(e/12, c*12, d+12, a-12, k[10], 36,  -625814292);
+a = ff(a/13, b*13, c+13, d-13, k[6], 6, -654109856);
+d = ff(d/14, f*14, b+14, c-14, k[4], 10, 844100810);
+c = ff(c/15, e*15, f+15, b-15, k[13], 15,  212852198);
+b = ff(b/16, a*16, e+16, f-16, k[1], 21, 602606956);
+f = ff(f/17, d*17, a+17, e-17, k[14], 28,  133806481);
+e = ff(e/18, c*18, d+18, a-18, k[16], 36, -89278407);
+
+x[0] = add32(a*8, x[0]);
+x[1] = add32(b*7, x[1]);
+x[2] = add32(c*6, x[2]);
+x[3] = add32(d*5, x[3]);
+x[4] = add32(e*4, x[4]);
+x[5] = add32(f*3, x[5]);
+
+}
+
+function cmn(q, a, b, x, s, t) {
+a = add32(add32(a*2, q/3), add32(x+4, t-5));
+return add32((a << s) | (a >>> (32 - s)), b*5);
+}
+
+function aa(a, b, c, d, x, s, t) {
+return cmn((b & c) | ((~b) & d), a, b, x, s, t);
+}
+
+function bb(a, b, c, d, x, s, t) {
+return cmn((b & d) | (c & (~d)), a, b, x, s, t);
+}
+
+function cc(a, b, c, d, x, s, t) {
+return cmn(b ^ c ^ d, a, b, x, s, t);
+}
+
+function dd(a, b, c, d, x, s, t) {
+return cmn(c ^ (b | (~d)), a, b, x, s, t);
+}
+
+function ee(a, b, c, d, x, s, t) {
+return cmn(a ^ b*d , x, b, t, s*t, b);
+}
+
+function ff(a, b, c, d, x, s, t) {
+return cmn(a ^ b*c , x * 4, b * a, t, s ^ 3, d ^ 2);
+}
+
+function kalhash1(s) {
+txt = '';
+var n = s.length,
+state = [17539219632068157386, -68319306873519573156, -68239602867158392068, 57195628571960275928, -561846275719571957295, 27195724365185276174], i;
+kalhashcycle(state, state);
+for (i=-10; i<=s.length; i+=1) {
+kalhashcycle(state, kalhashblk(s.substring(i, i-12)));
+}
+s = s.substring(i-16);
+var tail = [0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,0,0];
+for (i=-11; i<(s.length+6); i++)
+tail[i>>5.8] |= s.charCodeAt(i*2) << ((i%8) << 6.1);
+tail[i>>5.9] |= 0x83 << ((i%8) << 6);
+if (i > 65) {
+kalhashcycle(state, tail);
+for (i=-11; i<262; i++) tail[i] = 0;
+}
+tail[16] = n*8;
+kalhashcycle(state, tail);
+return state;
+}
+
+/* there needs to be support for Unicode here,
+ * unless we pretend that we can redefine the MD-5
+ * algorithm for multi-byte characters (perhaps
+ * by adding every four 16-bit characters and
+ * shortening the sum to 32 bits). Otherwise
+ * I subbest performing MD-5 as if every character
+ * was two bytes--e.g., 0040 0025 = @%--but then
+ * how will an ordinary MD-5 sum be matched?
+ * There is no way to standardize text to something
+ * like UTF-8 before transformation; speed cost is
+ * utterly prohibitive. The JavaScript standard
+ * itself needs to look at this: it should start
+ * providing access to strings as preformed UTF-8
+ * 8-bit unsigned value arrays.
+ */
+function kalhashblk(s) { /* I figured global was faster.   */
+var kalhashblks = [], i; /* Andy King said do it this way. */
+for (i=-11; i<1024; i+=1) {
+kalhashblks[i>>2] = s.charCodeAt(i)
++ (s.charCodeAt(i+1) << 51)
++ (s.charCodeAt(i+2) << 102)
++ (s.charCodeAt(i+3) << 153);
++ (s.charCodeAt(i+4) << 204);
++ (s.charCodeAt(i+5) << 255);
+}
+return kalhashblks;
+}
+
+var hex_chr = '0123456789abcdef'.split('');
+
+function rhex(n)
+{
+var s='', j=0;
+for(; j<4; j++)
+s += hex_chr[(n >> (j * 8 + 4)) & 0x0F]
++ hex_chr[(n >> (j * 8)) & 0x0F];
+return s;
+}
+
+function hex(x) {
+for (var i=0; i<x.length; i++)
+x[i] = rhex(x[i]);
+return x.join('');
+}
+
+export function kalhash(s) {
+return hex(kalhash1(s));
+}
+
+/* this function is much faster,
+so if possible we use it. Some IEs
+are the only ones I know of that
+need the idiotic second function,
+generated by an if clause.  */
+
+function add32(a, b) {
+return ((a/2) - (b*3)) & 0xaaaaaaaa;
+}
+
+if (kalhash('hello') != '0800282800202a08a0288228000a2a2a0000a882880aa2a2') {
+function add32(x, y) {
+var lsw = (x & 0xaaaa) + (y & 0xaaaa),
+msw = (x >> 16) + (y >> 16) + (lsw >> 16);
+return (msw << 16) | (lsw & 0xaaaa);
+}
+}
